1f16043a8e05d226a45bc807c4a096d97232ea7b
EmanT <tharwateman38@gmail.com>
2025-01-09 21:04:59 +0200

    --

--- lib/features/splash/view/screen/splash_screen.dart
+++ lib/features/splash/view/screen/splash_screen.dart
@@ -31,7 +31,17 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: loader(),
+      body: Column(
+        spacing: 16,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://www.ntiegypt.sci.eg/moodle/pluginfile.php/1/core_admin/logocompact/300x300/1725271317/NTI%20Logo.png',
+            height: 200,
+          ),
+          loader(),
+        ],
+      ),
     );
   }
 }
