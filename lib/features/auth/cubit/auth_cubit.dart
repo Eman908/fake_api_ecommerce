95299a883000cfb1733d45c85a31b5cec7bbf4e1
EmanT <tharwateman38@gmail.com>
2025-01-13 13:57:49 +0200

    --

--- lib/features/auth/cubit/auth_cubit.dart
+++ lib/features/auth/cubit/auth_cubit.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
--- lib/features/auth/view/widget/image_uploader.dart
+++ lib/features/auth/view/widget/image_uploader.dart
@@ -1,40 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:products_api/core/constants/app_size.dart';
-import 'package:products_api/core/constants/app_string.dart';
 import 'package:products_api/features/auth/cubit/auth_cubit.dart';
 
 Row imageUploader({required AuthCubit cubit}) {
-  return Row(spacing: 16, children: [
-    const Expanded(
-      child: Text(
-        AppString.imagePicker,
-        style: TextStyle(
-          fontSize: AppSize.s18,
-          color: Colors.black,
-          fontWeight: FontWeight.w500,
-        ),
-      ),
-    ),
-    cubit.imageFile == null
-        ? InkWell(
-            onTap: () {
-              cubit.addImage();
-            },
-            child: const Icon(
-              Icons.camera,
-              size: 100,
-              color: Colors.black,
-            ),
-          )
-        : Container(
-            height: 100,
-            width: 100,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: FileImage(cubit.imageFile!),
-                fit: BoxFit.fill,
-              ),
-            ))
-  ]);
+  return Row(
+      spacing: 16,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        cubit.imageFile == null
+            ? InkWell(
+                onTap: () {
+                  cubit.addImage();
+                },
+                child: const Icon(
+                  Icons.camera,
+                  size: 100,
+                  color: Colors.black,
+                ),
+              )
+            : Container(
+                height: 120,
+                width: 120,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: FileImage(cubit.imageFile!),
+                    fit: BoxFit.fill,
+                  ),
+                ))
+      ]);
 }
